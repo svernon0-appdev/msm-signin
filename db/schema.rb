e2cdfbe106362d8c8c71d2b1ccf9a346e14a4f12
Spencer Vernon <svernon0@chicagobooth.edu>
2023-05-15 18:46:55 +0000

    Add directors

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_15_184558) do
+ActiveRecord::Schema.define(version: 2023_05_15_184634) do
 
   create_table "actors", force: :cascade do |t|
+    t.string "name"
+    t.date "dob"
+    t.text "bio"
+    t.string "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "directors", force: :cascade do |t|
     t.string "name"
     t.date "dob"
     t.text "bio"
